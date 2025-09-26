6e8eabf299a0caebbace84dd5f2eb32c94450113
Rohit Maurya <rohit139maurya@gmail.com>
2025-09-26 18:37:02 +0530

    Update userDataContext.js

--- Frontend/src/context/userDataContext.js
+++ Frontend/src/context/userDataContext.js
@@ -4,4 +4,4 @@ import { io } from "socket.io-client";
 
 export const UserData = createContext()
 
-export const socket = io("http://localhost:4000");
+export const socket = io("https://netwise-webapp.onrender.com/");
